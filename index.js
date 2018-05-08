e37baa15a2a5a446748a5b7cdea19755ec0b1d5f
Jennifer Dobak <jdobak1@gmail.com>
2018-05-08 02:20:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -47,30 +47,11 @@ class Passenger {
 class Trip {
   constructor(passenger, driver) {
     this.id = ++tripId
-    this.passenger = passenger.id
-    this.driver = driver.id
+    this.passengerId = passenger.id
+    this.driverId = driver.id
     store.trips.push(this)
   }
-//
-//   driver(){
-//     return store.drivers.find(driver => {
-//       return driver.id === this.driverId
-//     })
-//   }
-//
-//   passenger(){
-//     return store.passengers.find(passenger => {
-//       return passenger.id === this.passengerId
-//     })
-//   }
-//
-// class Trip {
-  // constructor(driver, passenger) {
-  //   this.driverId = driver.id;
-  //   this.passengerId = passenger.id;
-  //   this.id = tripId++;
-  //   store.trips.push(this);
-  // }
+
   driver() {
     return store.drivers.find(driver => {
       return driver.id === this.driverId;
